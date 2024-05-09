957479f68149b6cef15399cb3325df6d224990c0
Jo@o_P@ul0SA <jpsantosdealmeida@hotmail.com>
2024-05-08 22:11:49 -0300

    Commit finalização cap 04 e inicio cap 05

---
+++ capitulo_05/cap_05_repeticoes.py
@@ -0,0 +1,9 @@
+# WHILE
+# while <condição>: repete o bloco enquanto essa condição for verdadeira
+#   bloco
+
+x = 1
+while x <= 3:  # enquanto x for menor ou igual a 3, printa x que é 1 e soma com ele com ele mesmo 
+    print(x)
+    x += 1
+    
